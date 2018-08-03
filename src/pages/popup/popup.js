60f74139e44d0540caaff133c249ccd82215e094
Ryan Thaut <rthaut@gmail.com>
2018-08-02 20:24:09 -0600

    popup page enhancements

--- src/pages/popup/popup.js
+++ src/pages/popup/popup.js
@@ -29,25 +29,81 @@ $('a[href^=\\#]').on('click', function (e) {
 });
 
 $('a#create_config').on('click', function (e) {
-    browser.windows.create({
-        'type': 'popup',
-        'url': browser.extension.getURL('/pages/create/create.html')
+    browser.runtime.sendMessage({
+        'action': 'open-config-editor'
     });
 });
 
-$('a#edit_config').on('click', function (e) {
-    const params = (new URL(document.location)).searchParams;
-    const UUID = params.get('UUID');
-    browser.windows.create({
-        'type': 'popup',
-        'url': browser.extension.getURL('/pages/create/create.html') + '?UUID=' + UUID
+function showConfigActionConfirmBar(configUUID, style, text, confirm) {
+    const item = $(`.panel-list-item[data-config-uuid='${configUUID}']`).first();
+
+    if (item !== undefined && item !== null) {
+        let overlay = $(item).children('.overlay').first();
+
+        if (overlay === undefined || overlay === null || !overlay.length) {
+            overlay = $('<div class="overlay"></div>').appendTo(item);
+        }
+
+        overlay.addClass(style);
+
+        overlay.append(`<div class="text">${text}</div>`);
+        overlay.append('<div class="button-bar"><a class="button" data-confirm="yes">Yes</a><a class="button" data-confirm="no">No</a></div>');
+
+        overlay.find('a[data-confirm]').on('click', async function (e) {
+            const confirmation = $(this).data('confirm').toLowerCase();
+            switch (confirmation) {
+                case 'yes':
+                    await confirm();
+                    break;
+
+                case 'no':
+                    break;
+            }
+
+            overlay.removeClass('visible');
+            setTimeout(function () {
+                overlay.remove();
+            }, 200);
+        });
+
+        // this causes the "open" transition to work; without it, the overlay just appears instantly...
+        setTimeout(function () {
+            overlay.addClass('visible');
+        }, 0);
+    }
+}
+
+async function deleteConfig(configUUID) {
+    await browser.runtime.sendMessage({
+        'action': 'delete-config',
+        'data': {
+            'configUUID': configUUID
+        }
     });
-});
+
+    const item = $(`.panel-list-item[data-config-uuid='${configUUID}']`).first();
+
+    item.remove();
+}
+
+async function toggleConfig(configUUID) {
+    const config = await browser.runtime.sendMessage({
+        'action': 'toggle-config',
+        'data': {
+            'configUUID': configUUID
+        }
+    });
+
+    const item = $(`.panel-list-item[data-config-uuid='${configUUID}']`).first();
+    const enabled = (config.Enabled === true);
+
+    item.toggleClass('text-muted', !enabled);
+    item.find('a[data-action="toggle"]').toggleClass('button-disable', enabled).toggleClass('button-enable', !enabled);
+}
 
 (async () => {
     // get values for the toggle settings from synced storage and assign default values where applicable
-    const settings = Object.assign(
-        {},
+    const settings = Object.assign({},
 
         // these are the defaults
         {
@@ -62,8 +118,6 @@ $('a#edit_config').on('click', function (e) {
             'PreloadEnabled'
         ])
     );
-
-    console.log('Settings', settings);
 
     Object.keys(settings).map((s) => {
         const toggle = $(`input[type='checkbox'][name='${s}']`);
@@ -99,12 +153,40 @@ $('a#edit_config').on('click', function (e) {
 
 (async () => {
     // get existing configurations
-    const { 'Configurations': configurations } = await browser.storage.sync.get('Configurations');
-    console.log('Configurations', configurations);
+    const {
+        'Configurations': configs
+    } = await browser.storage.sync.get('Configurations');
 
-    if (configurations.length) {
-        configurations.forEach((config) => {
-            $('#config_list').append(`<div class="panel-list-item"><div class="text">${config.Name}</div><div class="button button-edit"></div><div class="button button-delete"></div><div class="button button-disable"></div></div>`);
+    if (configs !== undefined && configs !== null && configs.length) {
+        configs.forEach((config) => {
+            const enabled = config.Enabled !== false;
+            $('#config_list').append(`<div class="panel-list-item ${enabled ? '' : 'text-muted'}" data-config-uuid="${config.UUID}"><div class="text">${config.Name}</div><div class="button-bar"><a class="button button-icon button-edit" data-action="edit"></a><a class="button button-icon button-delete" data-action="delete"></a><a class="button button-icon button-toggle ${enabled ? 'button-disable' : 'button-enable'}" data-action="toggle"></a></div></div>`);
+        });
+
+        // handle clicks on the buttons for individual configs
+        $('.panel-list-item[data-config-uuid] a.button[data-action]').on('click', function (e) {
+            const action = $(this).data('action').toLowerCase();
+            const configUUID = $(this).parents('[data-config-uuid]').data('config-uuid');
+            switch (action) {
+                case 'edit':
+                    browser.runtime.sendMessage({
+                        'action': 'open-config-editor',
+                        'data': {
+                            'configUUID': configUUID
+                        }
+                    });
+                    break;
+
+                case 'delete':
+                    showConfigActionConfirmBar(configUUID, 'bg-danger', 'Are you sure?', function () {
+                        deleteConfig(configUUID);
+                    });
+                    break;
+
+                case 'toggle':
+                    toggleConfig(configUUID);
+                    break;
+            }
         });
     }
 })();
